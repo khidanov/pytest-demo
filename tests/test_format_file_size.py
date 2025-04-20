62f0f92ac429d039a8c5e53fd45b4f096fe8eccd
Aleksei Khindanov <khindalex@gmail.com>
2025-04-20 18:57:31 -0500

    Adding files.

---
+++ tests/test_format_file_size.py
@@ -0,0 +1,19 @@
+import pytest
+from src.formatter1 import format_file_size
+
+
+@pytest.mark.parametrize(
+    "size_bytes, expected_result",
+    [
+        (0, "0B"),
+        (1, "1.00 B"),
+        (1024, "1.00 KB"),
+        (1024**2, "1.00 MB"),
+        (1024**3, "1.00 GB"),
+        (1024**4, "1.00 TB"),
+    ],
+)
+
+@pytest.mark.timeout(5)  # Set a 5-second timeout
+def test_format_file_size(size_bytes, expected_result):
+    assert format_file_size(size_bytes) == expected_result
