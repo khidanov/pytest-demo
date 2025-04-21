8183db782cd65c6d3d667ce6e22ed243ab93019b
Aleksei Khindanov <khindalex@gmail.com>
2025-04-20 20:06:43 -0500

    Adding numpy testing files 2.

---
+++ tests/test_generate_np_array.py
@@ -0,0 +1,22 @@
+import pytest
+from src.formatter1 import generate_np_array
+import numpy as np
+
+@pytest.mark.parametrize(
+    "size, expected_shape",
+    [
+        (1, (1,)),
+        (10, (10,)),
+        (100, (100,)),
+        (1000, (1000,)),
+        (10000, (10000,)),
+    ],
+)
+
+@pytest.mark.timeout(5)  # Set a 5-second timeout
+def test_generate_np_array(size, expected_shape):
+    result = generate_np_array(size)
+    assert result.shape == expected_shape
+    assert len(result) == size
+    assert isinstance(result, np.ndarray)
+    assert result.dtype == np.float64
